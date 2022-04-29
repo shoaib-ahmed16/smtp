cdd02cb6bb9919c67bc91bc34a53315e61d783b7
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-04-29 22:22:34 +0530

    asda

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
+// require('dotenv').config()
 module.exports = () => {
   return mongoose.connect(
-    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster1.xrndg.mongodb.net/emails-Shoaib?retryWrites=true&w=majority`
+    `mongodb+srv://shoaibEid03:t7jq3nlvRHhvPqZe@cluster1.xrndg.mongodb.net/emails-Shoaib?retryWrites=true&w=majority`
   )
 }
--- src/controllers/shoaib.controllers.js
+++ src/controllers/shoaib.controllers.js
@@ -1,12 +1,12 @@
 const path = require('path')
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 
 const transporter = require('../configs/mail')
 
 const Shoaib = require('../models/shoaib.model')
 
-const { body, validationResult } = require('express-validator')
+// const { body, validationResult } = require('express-validator')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
@@ -32,36 +32,21 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.post(
-  '/',
-  body('name').not().isEmpty().withMessage('name cannot be empty').bail(),
-  body('email')
-    .not()
-    .isEmpty()
-    .withMessage('email cannot be empty')
-    .isEmail()
-    .withMessage('not a valid email')
-    .bail(),
-  body('subject').not().isEmpty().withMessage('subject cannot be empty').bail(),
-  body('text').not().isEmpty().withMessage('Text cannot be empty').bail(),
-  async (req, res) => {
-    try {
-      const shoaib = await Shoaib.create(req.body)
+router.post('/', async (req, res) => {
+  try {
+    const shoaib = await Shoaib.create(req.body)
 
-      transporter.sendMail({
-        from: shoaib.email,
-        to: process.env.TOEMAIL,
-        subject: shoaib.subject,
-        text: `From:${shoaib.name},Email: ${shoaib.email},Text For Me: ${shoaib.text}`,
-      })
-      return res
-        .status(201)
-        .send({ message: 'Email is Recieve By Shoaib Ahmed' })
-    } catch (err) {
-      return res.status(500).send({ message: err.message })
-    }
+    transporter.sendMail({
+      from: shoaib.email,
+      to: 'sahmed094@gmail.com',
+      subject: shoaib.subject,
+      text: `From:${shoaib.name},Email: ${shoaib.email},Text For Me: ${shoaib.text}`,
+    })
+    return res.status(201).send({ message: 'Email is Recieve By Shoaib Ahmed' })
+  } catch (err) {
+    return res.status(500).send({ message: err.message })
   }
-)
+})
 
 router.get('/', async (req, res) => {
   try {
