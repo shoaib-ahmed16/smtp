a628ae8a80c91deacc6c0e610e572b480bcfdc3c
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-05-06 10:35:49 +0530

    asdas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./index')
 const connect = require('./configs/db')
 
-const port = 3000 || 3001 || 3002 || 3003
+const port = 3000 || 5500 || 5000 || 3500
 app.listen(port, async function () {
   await connect()
   console.log(`listening on port ${port}`)
