3a413a988d9514d2e30873fb0e77796ff873397f
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-05-16 17:41:01 +0530

    ad

--- src/configs/mail.js
+++ src/configs/mail.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer')
 require('dotenv').config()
 module.exports = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.SMTPUSER, // generated ethereal user
     pass: process.env.SMTPPASSWORD, // generated ethereal password
