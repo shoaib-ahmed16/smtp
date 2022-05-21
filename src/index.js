0eaa1ce77369c9b177dceda1b2ca5637b961f09b
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-05-21 13:19:13 +0530

    add no cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
 const shoaibController = require('./controllers/shoaib.controllers')
-
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/email', shoaibController)
